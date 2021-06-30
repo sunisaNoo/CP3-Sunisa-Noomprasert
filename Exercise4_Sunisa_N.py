410e5d27be603233c25d99dcaf1db71752e8334b
sunisaNoo <sunisa.taii@gmail.com>
2021-06-30 14:24:57 +0700

    Exercise4 print score

---
+++ Exercise4_Sunisa_N.py
@@ -0,0 +1,10 @@
+foundationEnglish = 80
+generalBusiness = 75
+introductionToComputerSystems = 85.5
+computerProgramming = 78.5
+
+print("---Your Score---")
+print("Foundation English :",foundationEnglish)
+print("General Business :",generalBusiness)
+print("Introduction to Computer System :",introductionToComputerSystems)
+print("Computer Programming :",computerProgramming)
